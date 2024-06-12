2c08c0df285d9c413c03717d1a73669ae38d57d2
sia <PXY23975814@outlook.com>
2024-06-12 21:32:58 +0800

    init

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import "@/app/ui/global.css";
+import { inter } from '@/app/ui/fonts';
+import { Children } from "react";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/* <body>{children}</body> */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
